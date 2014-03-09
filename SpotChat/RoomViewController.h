779ea149a25fd7208976eaecb3b617ffb122d0c7
Eric Strode <estrode@usc.edu>
2014-03-08 16:14:21 -0800

    Add navigation viewcontroller and chat room table view to storyboard.

---
+++ SpotChat/RoomViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoomViewController.h
+//  SpotChat
+//
+//  Created by ansan on 3/8/14.
+//  Copyright (c) 2014 Strocca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoomViewController : UITableViewController
+
+@end
